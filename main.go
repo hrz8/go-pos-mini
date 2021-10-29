5fec5b5d7faf5c21efa123afcfa268e9f07e397e
Hirzi Nurfakhrian <hirzinurfakhrian@gmail.com>
2021-10-29 14:44:30 +0700

    inject user domain into server 💉

--- main.go
+++ main.go
@@ -4,22 +4,51 @@ import (
 	"fmt"
 	"net/http"
 
+	UserREST "github.com/hrz8/go-pos-mini/domains/user/delivery/rest"
+	UserRepository "github.com/hrz8/go-pos-mini/domains/user/repository"
+	UserUsecase "github.com/hrz8/go-pos-mini/domains/user/usecase"
+
 	Config "github.com/hrz8/go-pos-mini/config"
+	Database "github.com/hrz8/go-pos-mini/database"
+	"github.com/hrz8/go-pos-mini/utils"
 	"github.com/labstack/echo/v4"
 	"github.com/labstack/echo/v4/middleware"
 )
 
 func main() {
 	appConfig := Config.NewConfig()
+	mysql := Database.NewMysql(appConfig)
+	mysqlSess := mysql.Connect()
 
-	restServer := echo.New()
+	RESTServer := echo.New()
+	RESTServer.Validator = utils.NewValidator()
 
 	// middlewares
-	restServer.Use(middleware.Logger())
-	restServer.Pre(middleware.RemoveTrailingSlash())
+	RESTServer.Use(middleware.Logger())
+	RESTServer.Pre(middleware.RemoveTrailingSlash())
+	RESTServer.Use(func(next echo.HandlerFunc) echo.HandlerFunc {
+		return func(ctx echo.Context) error {
+			cc := &utils.CustomContext{
+				Context:   ctx,
+				MysqlSess: mysqlSess,
+				AppConfig: appConfig,
+			}
+			return next(cc)
+		}
+	})
 
-	restServer.GET("/", func(c echo.Context) error {
+	RESTServer.GET("/", func(c echo.Context) error {
 		return c.String(http.StatusOK, "Hello, World!")
 	})
-	restServer.Logger.Fatal(restServer.Start(fmt.Sprintf(":%d", appConfig.SERVICE.RESTPORT)))
+
+	// domains functions
+	userRepository := UserRepository.NewRepository(mysqlSess)
+	userUsecase := UserUsecase.NewUsecase(userRepository)
+	userREST := UserREST.NewRest(userUsecase)
+
+	// register REST endpoints
+	UserREST.AddUserEndpoints(RESTServer, userREST)
+
+	// serve REST
+	RESTServer.Logger.Fatal(RESTServer.Start(fmt.Sprintf(":%d", appConfig.SERVICE.RESTPORT)))
 }

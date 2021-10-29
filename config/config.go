a41c8f23d757c1bc61c83800627bb3815e5932dc
Hirzi Nurfakhrian <hirzinurfakhrian@gmail.com>
2021-10-29 09:50:16 +0700

    add config file and handlers 🔐

---
+++ config/config.go
@@ -0,0 +1,49 @@
+package config
+
+import (
+	"log"
+	"sync"
+
+	"github.com/spf13/viper"
+)
+
+type (
+	SERVICE struct {
+		RESTPORT  int    `mapstructure:"RESTPORT"`
+		JWTSECRET string `mapstructure:"JWTSECRET"`
+	}
+	DATABASE struct {
+		HOST     string `mapstructure:"HOST"`
+		PORT     int    `mapstructure:"PORT"`
+		USER     string `mapstructure:"USER"`
+		PASSWORD string `mapstructure:"PASSWORD"`
+		NAME     string `mapstructure:"NAME"`
+	}
+	AppConfig struct {
+		SERVICE  SERVICE  `mapstructure:"SERVICE"`
+		DATABASE DATABASE `mapstructure:"DATABASE"`
+	}
+)
+
+var (
+	once      sync.Once
+	appConfig AppConfig
+)
+
+// NewConfig return configurations implementation
+func NewConfig() *AppConfig {
+	once.Do(func() {
+		v := viper.New()
+
+		v.AddConfigPath(".")
+		v.SetConfigName("config")
+		v.SetConfigType("yml")
+
+		if err := v.ReadInConfig(); err != nil {
+			log.Fatal("Failed to read config file")
+		}
+
+		v.Unmarshal(&appConfig)
+	})
+	return &appConfig
+}
--- main.go
+++ main.go
@@ -1,15 +1,25 @@
 package main
 
 import (
+	"fmt"
 	"net/http"
 
+	Config "github.com/hrz8/go-pos-mini/config"
 	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
 )
 
 func main() {
-	e := echo.New()
-	e.GET("/", func(c echo.Context) error {
+	appConfig := Config.NewConfig()
+
+	restServer := echo.New()
+
+	// middlewares
+	restServer.Use(middleware.Logger())
+	restServer.Pre(middleware.RemoveTrailingSlash())
+
+	restServer.GET("/", func(c echo.Context) error {
 		return c.String(http.StatusOK, "Hello, World!")
 	})
-	e.Logger.Fatal(e.Start(":1323"))
+	restServer.Logger.Fatal(restServer.Start(fmt.Sprintf(":%d", appConfig.SERVICE.RESTPORT)))
 }

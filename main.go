aa70bac329ea1190b3b075787b515919da0f942a
Hirzi Nurfakhrian <hirzinurfakhrian@gmail.com>
2021-10-29 17:01:15 +0700

    add jwt middleware to check credential 🔐

--- main.go
+++ main.go
@@ -1,11 +1,15 @@
 package main
 
 import (
+	"errors"
 	"fmt"
 
+	"github.com/golang-jwt/jwt"
 	UserREST "github.com/hrz8/go-pos-mini/domains/user/delivery/rest"
+	DomainUserError "github.com/hrz8/go-pos-mini/domains/user/error"
 	UserRepository "github.com/hrz8/go-pos-mini/domains/user/repository"
 	UserUsecase "github.com/hrz8/go-pos-mini/domains/user/usecase"
+	"github.com/hrz8/go-pos-mini/models"
 
 	Config "github.com/hrz8/go-pos-mini/config"
 	Database "github.com/hrz8/go-pos-mini/database"
@@ -18,6 +22,11 @@ func main() {
 	appConfig := Config.NewConfig()
 	mysql := Database.NewMysql(appConfig)
 	mysqlSess := mysql.Connect()
+
+	// domains functions
+	userRepository := UserRepository.NewRepository(mysqlSess)
+	userUsecase := UserUsecase.NewUsecase(userRepository)
+	userREST := UserREST.NewRest(userUsecase)
 
 	RESTServer := echo.New()
 	RESTServer.Validator = utils.NewValidator()
@@ -35,14 +44,36 @@ func main() {
 			return next(cc)
 		}
 	})
-
-	// domains functions
-	userRepository := UserRepository.NewRepository(mysqlSess)
-	userUsecase := UserUsecase.NewUsecase(userRepository)
-	userREST := UserREST.NewRest(userUsecase)
+	// jwt middleware handler
+	JWTConfig := middleware.JWTConfig{
+		Claims:     &models.UserJwt{},
+		SigningKey: []byte(appConfig.SERVICE.JWTSECRET),
+		ParseTokenFunc: func(token string, c echo.Context) (interface{}, error) {
+			ctx := c.(*utils.CustomContext)
+			parsedJwt, err := jwt.Parse(token, func(token *jwt.Token) (interface{}, error) {
+				if jwt.GetSigningMethod("HS256") != token.Method {
+					return nil, errors.New("unexpected signing method")
+				}
+				return []byte(appConfig.SERVICE.JWTSECRET), nil
+			})
+			if err != nil || parsedJwt == nil {
+				return nil, errors.New("invalid token")
+			}
+			userEmail := parsedJwt.Claims.(jwt.MapClaims)["aud"].(string)
+			user, err := userRepository.GetBy(nil, &models.User{Email: userEmail})
+			if err != nil {
+				return nil, err
+			}
+			if user == nil {
+				return nil, DomainUserError.GetBy.Err
+			}
+			ctx.User = user
+			return user, nil
+		},
+	}
 
 	// register REST endpoints
-	UserREST.AddUserEndpoints(RESTServer, userREST)
+	UserREST.AddUserEndpoints(RESTServer, userREST, &JWTConfig)
 
 	// serve REST
 	RESTServer.Logger.Fatal(RESTServer.Start(fmt.Sprintf(":%d", appConfig.SERVICE.RESTPORT)))
